2226f0c15630f2eb8a46a0ae8701ddcc911a5153
Steven Schobert <spschobert@gmail.com>
2014-06-09 22:13:54 -0500

    single quotes instead of doubles
    because I can’t ever make up my mind about this stuff

--- test/test.js
+++ test/test.js
@@ -1,28 +1,28 @@
 (function () {
-  "use strict";
+  'use strict';
 
-  var each = require("async").each,
-      join = require("path").join,
-      metalsmith = require("metalsmith"),
-      rm = require("rimraf"),
-      sass = require(".."),
-      equal = require("assert-dir-equal");
+  var each = require('async').each,
+      join = require('path').join,
+      metalsmith = require('metalsmith'),
+      rm = require('rimraf'),
+      sass = require('..'),
+      equal = require('assert-dir-equal');
 
-  describe("the plugin", function () {
+  describe('the plugin', function () {
     beforeEach(function (done) {
       var dirsToClean = [
-        join(__dirname, "fixtures/basic/build"),
-        join(__dirname, "fixtures/partials/build"),
-        join(__dirname, "fixtures/outputDir/build")
+        join(__dirname, 'fixtures/basic/build'),
+        join(__dirname, 'fixtures/partials/build'),
+        join(__dirname, 'fixtures/outputDir/build')
       ];
       each(dirsToClean, rm, done);
     });
 
-    describe("core", function () {
-      it("should compile sass files", function (done) {
+    describe('core', function () {
+      it('should compile sass files', function (done) {
         metalsmith(__dirname)
-          .source("fixtures/basic/src")
-          .destination("fixtures/basic/build")
+          .source('fixtures/basic/src')
+          .destination('fixtures/basic/build')
           .use(sass({
             outputStyle: 'expanded'
           }))
@@ -30,15 +30,15 @@
             if (err) {
               throw err;
             }
-            equal(join(__dirname, "fixtures/basic/build"), join(__dirname, "fixtures/basic/expected"));
+            equal(join(__dirname, 'fixtures/basic/build'), join(__dirname, 'fixtures/basic/expected'));
             done();
           });
       });
 
-      it("should ignore partial files", function (done) {
+      it('should ignore partial files', function (done) {
         metalsmith(__dirname)
-          .source("fixtures/partials/src")
-          .destination("fixtures/partials/build")
+          .source('fixtures/partials/src')
+          .destination('fixtures/partials/build')
           .use(sass({
             outputStyle: 'expanded'
           }))
@@ -46,17 +46,17 @@
             if (err) {
               throw err;
             }
-            equal(join(__dirname, "fixtures/partials/build"), join(__dirname, "fixtures/partials/expected"));
+            equal(join(__dirname, 'fixtures/partials/build'), join(__dirname, 'fixtures/partials/expected'));
             done();
           });
       });
     });
 
-    describe("the outputDir option", function () {
-      it("should change the destination directory", function (done) {
+    describe('the outputDir option', function () {
+      it('should change the destination directory', function (done) {
         metalsmith(__dirname)
-          .source("fixtures/outputDir/src")
-          .destination("fixtures/outputDir/build")
+          .source('fixtures/outputDir/src')
+          .destination('fixtures/outputDir/build')
           .use(sass({
             outputStyle: 'expanded',
             outputDir: 'nested/'
@@ -65,7 +65,7 @@
             if (err) {
               throw err;
             }
-            equal(join(__dirname, "fixtures/outputDir/build"), join(__dirname, "fixtures/outputDir/expected"));
+            equal(join(__dirname, 'fixtures/outputDir/build'), join(__dirname, 'fixtures/outputDir/expected'));
             done();
           });
       });
